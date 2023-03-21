44302ddf82a328f685ac5112f4349055e7423c9a
rlucente <rlucente@redhat.com>
2023-03-21 16:12:06 -0400

    cleanup and reorganize

--- scripts/test-ws.sh
+++ scripts/test-ws.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+. $(dirname $0)/../demo.conf
+
+[[ $EUID -eq 0 ]] && exit_on_error "Must NOT run as root"
+
 while true
 do
-    curl -s http://localhost:8080/ads-b-states | jq .
+    curl -vs http://localhost:${WS_PORT}/ads-b-states | jq .
     echo
     sleep 1
 done
