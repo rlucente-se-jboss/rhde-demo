3c01d5ed3b6ecf9158ee6c5c879bc4e65f96e481
rlucente <rlucente@redhat.com>
2023-04-19 16:46:49 -0400

    update to latest ui container

--- scripts/prepare-blueprint-and-ks.sh
+++ scripts/prepare-blueprint-and-ks.sh
@@ -32,7 +32,7 @@ version = "4.12.6"
 # source = "quay.io/redhatgov/ads-b-service:v0.0.5"
 #
 # [[containers]]
-# source = "quay.io/jasonredhat/ads-b-map:v0.1.4"
+# source = "quay.io/jasonredhat/ads-b-map:v0.1.5"
 
 [customizations.services]
 enabled = ["microshift"]
